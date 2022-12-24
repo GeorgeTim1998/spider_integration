3a506ace7b8f1cf1c56c5bdf0bee73c0debff47f
georgetim1998 <georgetim1998@gmail.com>
2022-12-24 22:03:20 +0300

    download files refactor

--- load_data_from_matlab_scripts.py
+++ load_data_from_matlab_scripts.py
@@ -1,29 +1,31 @@
+import numpy as np
+
 class LoadDataFromMatlabScripts:
-  MATLAB_SCRIPTS_OUTPUT = ['matlab_scripts_output/20221223/r.txt',
-                           'matlab_scripts_output/20221223/z.txt',
-                           'matlab_scripts_output/20221223/rxb.txt',
-                           'matlab_scripts_output/20221223/zxb.txt',
-                           'matlab_scripts_output/20221223/psi.txt']
+  ARRAYS_FILES = ['matlab_scripts_output/20221223/r.txt',
+                   'matlab_scripts_output/20221223/z.txt',
+                   'matlab_scripts_output/20221223/rxb.txt',
+                   'matlab_scripts_output/20221223/zxb.txt']
+  MATRIXES_FILES = ['matlab_scripts_output/20221223/psi.txt']
   
   def __init__(self):
-    text_file1 = open(LoadDataFromMatlabScripts.MATLAB_SCRIPTS_OUTPUT[0], "r")
-    text_file2 = open(LoadDataFromMatlabScripts.MATLAB_SCRIPTS_OUTPUT[1], "r")
-    text_file3 = open(LoadDataFromMatlabScripts.MATLAB_SCRIPTS_OUTPUT[2], "r")
-    text_file4 = open(LoadDataFromMatlabScripts.MATLAB_SCRIPTS_OUTPUT[3], "r")
+    self.r, self.z, self.rxb, self.zxb = self.load_arrays_from_m()
+    self.psi = self.load_matrixes_from_m()
     
-    self.r = text_file1.readlines()
-    self.z = text_file2.readlines()
-    self.rxb = text_file3.readlines()
-    self.zxb = text_file4.readlines()
-    self.psi = self.read_matrix(LoadDataFromMatlabScripts.MATLAB_SCRIPTS_OUTPUT[4])
+  def load_arrays_from_m(self):
+    data = []
+    for file_name in LoadDataFromMatlabScripts.ARRAYS_FILES:
+      file = open(file_name, "r")
+      data.append(np.fromfile(file, sep=','))
+      file.close()
     
-    text_file1.close()
-    text_file2.close()
-    text_file3.close()
-    text_file4.close()
+    return data
+  
+  def load_matrixes_from_m(self):
+    data = []
+    for file_name in LoadDataFromMatlabScripts.MATRIXES_FILES:
+      file = open(file_name, "r")
+      data.append(np.loadtxt(file, delimiter=','))
+      file.close()
     
-  def read_matrix(self, file_name):
-    with open(file_name, 'r') as f:
-      matrix = [[num for num in line.split(',')] for line in f]
-      
+    return data
     
