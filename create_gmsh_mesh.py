25565a5e74c08da619df40c66149ed314cce50be
georgetim1998 <georgetim1998@gmail.com>
2023-01-15 21:31:06 +0300

    import lao data

--- create_gmsh_mesh.py
+++ create_gmsh_mesh.py
@@ -1,13 +1,15 @@
 import gmsh
+import support as sup
 
-folder = 'Mesh/msh'
+folder = sup.msh_files_folder()
+lao_hash = sup.lao_hash()
 
 dim = 2
 mesh_size = 0.01
-ellipse_center = [0, 0, 0]
-ellipse_ratio = 3
-ellipse_a = 3
-ellipse_b = 1
+ellipse_center = [lao_hash['Rt'][0], 0, 0]
+ellipse_ratio = lao_hash['E'][0]
+ellipse_a = lao_hash['a']
+ellipse_b = ellipse_ratio * ellipse_a
 
 gmsh.initialize()
 gmsh.option.setNumber('Mesh.MeshSizeMax', mesh_size) # you can set general options via strings in set number. see https://gmsh.info/doc/texinfo/gmsh.pdf
@@ -22,9 +24,9 @@ model.addPhysicalGroup(dim, [surface]) # add physical group to surface (physical
 model.addPhysicalGroup(dim-1, [ellipse]) # add physical group to curve (physical groups needed for fenics)
 
 model.mesh.generate(dim)
+gmsh.fltk.run()
 
 gmsh.option.setNumber('Mesh.MshFileVersion', 2.2) # setting ascii 2 version so fenics understands whats up
 gmsh.write(folder + '/' + "filename.msh")
 
-gmsh.fltk.run()
 gmsh.finalize()
--- support.py
+++ support.py
@@ -12,3 +12,22 @@ def linear_profiles():
   psi0 = 1
   
   return p0, f0_2, psi0
+
+def msh_files_folder():
+  return 'Mesh/msh'
+
+def xml_files_folder():
+  return 'Mesh/xml'
+
+def lao_hash():
+  Rt = [142.5, 142.5, 142.6, 142.3] # sm
+  E =  [1.60, 1.59, 1.59, 1.60] # elongation
+  betta_p = [0.91, 0.91, 0.90, 0.92] # poloidal betta
+  a = 37
+  
+  return {
+    'Rt': Rt,
+    'E': E,
+    'betta_p': betta_p,
+    'a': a
+  }
