86bbf1e0236c8903634ad1cbd071ce2f136cb9e4
georgetim1998 <georgetim1998@gmail.com>
2023-01-15 16:43:15 +0300

    gmsh find max size

--- create_gmsh_mesh.py
+++ create_gmsh_mesh.py
@@ -5,6 +5,8 @@ folder = 'Mesh/msh'
 
 gmsh.initialize()
 
+gmsh.option.setNumber('Mesh.MeshSizeMax', 0.1)
+
 model = gmsh.model()
 sphere = model.occ.addEllipse(0, 0, 0, 2, 1, tag=5, zAxis=[0, 0, 1], xAxis=[0, 1, 0])
 
@@ -18,7 +20,12 @@ model.occ.synchronize()
 model.addPhysicalGroup(dim, [surface]) # add physical group to surface (physical group needed for fenics)
 model.addPhysicalGroup(dim-1, [sphere]) # add physical group to curve (physical group needed for fenics)
 
+# model.mesh.field.setNumber(2, "SizeMax", 1)
 model.mesh.generate(dim)
+
+
+# model.mesh.refine()
+# model.mesh.refine()
 
 gmsh.option.setNumber('Mesh.MshFileVersion', 2.2)   
 gmsh.write(folder + '/' + "filename.msh")
