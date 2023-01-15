32beacf85bf2ea539a0de31a74d9fe17b7072a66
georgetim1998 <georgetim1998@gmail.com>
2023-01-15 16:50:10 +0300

    refactor and cpmment

--- create_gmsh_mesh.py
+++ create_gmsh_mesh.py
@@ -3,31 +3,27 @@ import math
 
 folder = 'Mesh/msh'
 
-gmsh.initialize()
+dim=2
+mesh_size = 0.1
+ellipse_center = [0, 0, 0]
+ellipse_a = 3
+ellipse_b = 1
 
-gmsh.option.setNumber('Mesh.MeshSizeMax', 0.1)
+gmsh.initialize()
+gmsh.option.setNumber('Mesh.MeshSizeMax', mesh_size) # you can set general options via strings in set number. see https://gmsh.info/doc/texinfo/gmsh.pdf
 
 model = gmsh.model()
-sphere = model.occ.addEllipse(0, 0, 0, 2, 1, tag=5, zAxis=[0, 0, 1], xAxis=[0, 1, 0])
-
-dim=2
-
-face=model.occ.addCurveLoop([sphere]) # say that curve is closed
-surface = model.occ.addPlaneSurface([face]) # say that curve delimits surface
-
+ellipse = model.occ.addEllipse(*ellipse_center, ellipse_a, ellipse_b, zAxis=[0, 0, 1], xAxis=[0, 1, 0])
+loop = model.occ.addCurveLoop([ellipse]) # say that curve is closed/looped
+surface = model.occ.addPlaneSurface([loop]) # say that curve delimits surface
 model.occ.synchronize()
 
-model.addPhysicalGroup(dim, [surface]) # add physical group to surface (physical group needed for fenics)
-model.addPhysicalGroup(dim-1, [sphere]) # add physical group to curve (physical group needed for fenics)
+model.addPhysicalGroup(dim, [surface]) # add physical group to surface (physical groups needed for fenics)
+model.addPhysicalGroup(dim-1, [ellipse]) # add physical group to curve (physical groups needed for fenics)
 
-# model.mesh.field.setNumber(2, "SizeMax", 1)
 model.mesh.generate(dim)
 
-
-# model.mesh.refine()
-# model.mesh.refine()
-
-gmsh.option.setNumber('Mesh.MshFileVersion', 2.2)   
+gmsh.option.setNumber('Mesh.MshFileVersion', 2.2) # setting ascii 2 version so fenics understands whats up
 gmsh.write(folder + '/' + "filename.msh")
 
 gmsh.fltk.run()
