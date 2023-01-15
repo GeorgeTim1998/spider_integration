48a11af54d83762b632856e74c2f6329aa9d3e70
georgetim1998 <georgetim1998@gmail.com>
2023-01-15 21:48:14 +0300

    ready for first calc w/out plasma params

--- create_gmsh_mesh.py
+++ create_gmsh_mesh.py
@@ -5,7 +5,7 @@ folder = sup.msh_files_folder()
 lao_hash = sup.lao_hash()
 
 dim = 2
-mesh_size = 0.01
+mesh_size = 1
 ellipse_center = [lao_hash['Rt'][0], 0, 0]
 ellipse_ratio = lao_hash['E'][0]
 ellipse_a = lao_hash['a']
@@ -15,7 +15,7 @@ gmsh.initialize()
 gmsh.option.setNumber('Mesh.MeshSizeMax', mesh_size) # you can set general options via strings in set number. see https://gmsh.info/doc/texinfo/gmsh.pdf
 
 model = gmsh.model()
-ellipse = model.occ.addEllipse(*ellipse_center, ellipse_a, ellipse_b, zAxis=[0, 0, 1], xAxis=[0, 1, 0])
+ellipse = model.occ.addEllipse(*ellipse_center, ellipse_b, ellipse_a, zAxis=[0, 0, 1], xAxis=[0, 1, 0])
 loop = model.occ.addCurveLoop([ellipse]) # say that curve is closed/looped
 surface = model.occ.addPlaneSurface([loop]) # say that curve delimits surface
 model.occ.synchronize()
@@ -26,7 +26,8 @@ model.addPhysicalGroup(dim-1, [ellipse]) # add physical group to curve (physical
 model.mesh.generate(dim)
 gmsh.fltk.run()
 
+file_name = "a_%.3f_ratio_%.3f_msh_%.1e" % (ellipse_a, ellipse_ratio, mesh_size)
 gmsh.option.setNumber('Mesh.MshFileVersion', 2.2) # setting ascii 2 version so fenics understands whats up
-gmsh.write(folder + '/' + "filename.msh")
+gmsh.write("%s/%s.msh" % (folder, file_name))
 
 gmsh.finalize()
--- import_mesh_in_fenics.py
+++ import_mesh_in_fenics.py
@@ -1,7 +1,10 @@
 from fenics import *
 import matplotlib.pyplot as pyplot
+import support as sup
+import fenics_support as fsup
+from math import pi
 
-folder = 'Mesh/xml'
+folder = sup.xml_files_folder()
 xml_file = folder + '/' + 't1.xml'
 xml_file_facet = folder + '/' + 't1_facet_region.xml'
 xml_file_physical_region = folder + '/' + 't1_physical_region.xml'
@@ -9,23 +12,24 @@ xml_file_physical_region = folder + '/' + 't1_physical_region.xml'
 gmsh = Mesh(xml_file)
 
 V = FunctionSpace(gmsh, 'P', 1)
-
-# Define boundary condition
-u_D = Expression('1 + x[0]*x[0] + 2*x[1]*x[1]', degree=2)
+#%% Define boundary condition
+u_D = Expression('0', degree=2)
 
 def boundary(x, on_boundary):
     return on_boundary
 
 bc = DirichletBC(V, u_D, boundary)
-
-# Define variational problem
+#%% Define variational problem
 u = TrialFunction(V)
 v = TestFunction(V)
-f = Constant(-6.0)
-a = dot(grad(u), grad(v))*dx
-L = f*v*dx
 
-# Compute solution
+[r_2, r] = fsup.operator_weights(V)
+[p0, f0_2, psi0] = fsup.linear_profiles()
+
+a = dot(grad(u)/r, grad(r_2*v))*dx
+f = Constant( (4 * pi * p0 + 0.5 * f0_2) / psi0 )
+L = f*v*dx
+#%% Compute solution
 u = Function(V)
 solve(a == L, u, bc)
 
