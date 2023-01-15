fbbc4be780738bf1af91d0a8c1b7c89508b876ad
georgetim1998 <georgetim1998@gmail.com>
2023-01-15 15:56:23 +0300

    mesh in fenicsproject310

--- create_gmsh_mesh.py
+++ create_gmsh_mesh.py
@@ -1,12 +1,15 @@
-from fenics import *
 import gmsh
+import math
+
+folder = 'Mesh/msh'
 
 gmsh.initialize()
 
 model = gmsh.model()
-sphere = model.occ.addEllipse(0, 0, 0, 2, 1, tag=5)
+sphere = model.occ.addEllipse(0, 0, 0, 2, 1, tag=5, zAxis=[0, 0, 1], xAxis=[0, 1, 0])
 
 dim=2
+
 face=model.occ.addCurveLoop([sphere]) # say that curve is closed
 surface = model.occ.addPlaneSurface([face]) # say that curve delimits surface
 
@@ -16,6 +19,9 @@ model.addPhysicalGroup(dim, [surface]) # add physical group to surface (physical
 model.addPhysicalGroup(dim-1, [sphere]) # add physical group to curve (physical group needed for fenics)
 
 model.mesh.generate(dim)
+
+gmsh.option.setNumber('Mesh.MshFileVersion', 2.2)   
+gmsh.write(folder + '/' + "filename.msh")
+
 gmsh.fltk.run()
 gmsh.finalize()
-
