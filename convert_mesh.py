f4c45aef7fee90029f2afe15061fa35978cb32b6
georgetim1998 <georgetim1998@gmail.com>
2023-01-21 17:54:39 +0300

    fix bug

--- convert_mesh.py
+++ convert_mesh.py
@@ -8,7 +8,7 @@ xml_folder = sup.xml_files_folder()
 xml_files = sup.drop_files_ext(os.listdir(xml_folder))
 xml_files = sup.drop_physical_n_dacet_files(xml_files)
 
-if msh_files != xml_files:
+if msh_files.sort() != xml_files.sort():
   convert_indexes = []
   
   if len(xml_files) == 0:
