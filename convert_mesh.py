81a0eac1e81022a764b1ca1ad31576125a3eb1e3
georgetim1998 <georgetim1998@gmail.com>
2023-01-21 18:09:16 +0300

    update

--- convert_mesh.py
+++ convert_mesh.py
@@ -8,16 +8,24 @@ xml_folder = sup.xml_files_folder()
 xml_files = sup.drop_files_ext(os.listdir(xml_folder))
 xml_files = sup.drop_physical_n_dacet_files(xml_files)
 
-if msh_files.sort() != xml_files.sort():
+msh_files.sort()
+xml_files.sort()
+
+if msh_files != xml_files:
   convert_indexes = []
   
   if len(xml_files) == 0:
     convert_indexes = range(len(msh_files))
-    
+  
+  converted = 0
+  
   for i, xml in enumerate(xml_files):
     for j, msh in enumerate(msh_files):
       if msh != xml:
         convert_indexes.append(j)
+        converted += 1
+        print(converted)
+        
       
   for i in convert_indexes:
     bash_command = "dolfin-convert %s/%s.msh %s/%s.xml" % (msh_folder, msh_files[i], xml_folder, msh_files[i])
