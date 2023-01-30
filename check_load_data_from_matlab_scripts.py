b7d6e633d91e99feab633e0ba3ab442c2879f06c
georgetim1998 <georgetim1998@gmail.com>
2023-01-30 19:57:15 +0300

    ffff

--- check_load_data_from_matlab_scripts.py
+++ check_load_data_from_matlab_scripts.py
@@ -28,17 +28,10 @@ psi = Function(V)
 r = matlab_data.r
 z = matlab_data.z
 
-dof_indexes = vertex_to_dof_map(V) # this is probably what you need. it is an array of indexes of vertices. you need to do it like in your textbook!
-
 interpolant = interp2d(r, z, matlab_data.psi, kind='linear', copy=False, bounds_error=True)
 
-values = []
-# for dof_index in dof_indexes:
-#   coord = mesh.coordinates()[dof_index]
-#   values.append(interpolant(coord[0], coord[1]))
-for coord in gmsh.coordinates():
-  values.append(interpolant(coord[0], coord[1]))
-  
-psi.vector().set_local(values)
-plot(psi)
+expression = ExpressionFromScipyFunction(interpolant, element=V.ufl_element())
+expression = interpolate(expression, V) 
+# psi.vector().set_local(values)
+plot(expression)
 matplt.show()
