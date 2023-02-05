37749a9e1c35a97d33586ca6e11ed77cda5a6550
georgetim1998 <georgetim1998@gmail.com>
2023-02-05 10:28:54 +0300

    check stability

--- tryout.py
+++ tryout.py
@@ -1,2 +1,20 @@
-import matplotlib.pyplot as mp
-import fenics_support as fsup
+import numpy
+
+def solve_SLAE(alpha):
+  matrix = numpy.array(
+    [
+      [3,            1, -1],
+      [1,            1,  1],
+      [1, -(alpha - 1), -1]
+    ]
+  )
+  
+  f = numpy.array([3.28681356, 1.25700438, 0.99833036])
+  
+  x = numpy.linalg.solve(matrix, f)
+  
+  return x
+
+# alpha = 1.0226596521091218
+alpha = 1.0216596521091218
+print(solve_SLAE(alpha), 'alpha = ', alpha)
