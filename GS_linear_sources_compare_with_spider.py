fa910869c8c1339de04d5637c5e1ca472ec584e7
georgetim1998 <georgetim1998@gmail.com>
2023-02-21 18:08:30 +0300

    update

--- GS_linear_sources_compare_with_spider.py
+++ GS_linear_sources_compare_with_spider.py
@@ -85,8 +85,10 @@ for i, filename in enumerate(filenames):
   ppsi = project(p0 * u/psi0, V)
   F2psi = project(F2_0*(1 + Fpl_vs_Fvac_ratio*u/psi0), V)
   
-  print(ppsi.vector()[:].max())
-  print(F2psi.vector()[:].max())
+  print("psi_max = ", u.vector()[:].max())
+  print("pressure max =", ppsi.vector()[:].max())
+  print("F2 max =", F2psi.vector()[:].max())
+  print("F2 min = ", F2psi.vector()[:].min())
   
   fsup.countour_plot_via_mesh(gmsh, u, levels = 20, colorbar=True, grid=True, PATH=my_dir, plot_title="E = %.1f" % E[i])
   print("\n")
